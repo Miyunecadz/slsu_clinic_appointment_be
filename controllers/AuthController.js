c70608a7b6e30360d8e9b72df5b2ab77f3d0961f
miyunecadz <jvcadz@gmail.com>
2022-12-05 22:12:22 +0800

    Update Authentication

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,38 +4,45 @@ const PatientRepository = require('../repositories/Patient')
 const SpecialistRepository = require('../repositories/Specialist')
 
 const login = async (req, res) => {
-    const credentials = {
-        username: req.body.username,
-        password: await md5(req.body.password)
-    }
-
-    const accountResult = await AccountRepository.find(credentials)
-
-    if (!accountResult) {
+    try {
+        const credentials = {
+            username: req.body.username,
+            password: await md5(req.body.password)
+        }
+    
+        const accountResult = await AccountRepository.find(credentials)
+    
+        if (!accountResult) {
+            return res.json({
+                "result": false,
+                "message": "Invalid credentials"
+            })
+        }
+    
+        let user = null;
+    
+        if (accountResult.account_type == 1) {
+            user = { 'first_name': 'admin' }
+            user.account_type = 1
+        } else if (accountResult.account_type == 2) {
+            user = await SpecialistRepository.findById(accountResult.user_id)
+            user.account_type = 2
+        } else if (accountResult.account_type == 3) {
+            user = await PatientRepository.findById(accountResult.user_id)
+            user.account_type = 3
+        }
+    
         return res.json({
-            "result": false,
-            "message": "Invalid credentials"
+            "result": true,
+            "user": user
+        })
+    } catch (e) {
+        return res.json({
+            'result': false,
+            'message': 'Server error contact the administrator'
         })
     }
-
-    let user = null;
-
-    if (accountResult.account_type == 1) {
-        user = { 'first_name': 'admin' }
-        user.account_type = 1
-    } else if (accountResult.account_type == 2) {
-        user = await SpecialistRepository.findById(accountResult.user_id)
-        user.account_type = 2
-    } else if (accountResult.account_type == 3) {
-        user = await PatientRepository.findById(accountResult.user_id)
-        user.account_type = 3
-    }
-
-
-    return res.json({
-        "result": true,
-        "user": user
-    })
+    
 }
 
 module.exports = {
