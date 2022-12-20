0c6c1cc64614baf5777ae3138000664bbe01df7b
miyunecadz <jvcadz@gmail.com>
2022-12-20 12:49:50 +0800

    Update date time condition in schedules

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 const { md5 } = require('../helpers/hashing')
 const AccountRepository = require('../models/Account')
+const Admin = require('../models/Admin')
 const PatientRepository = require('../models/Patient')
 const SpecialistRepository = require('../models/Specialist')
 
@@ -19,22 +20,23 @@ const login = async (req, res) => {
             })
         }
     
-        let user = null;
+        let user = {};
     
         if (accountResult.account_type == 1) {
-            user = { 'first_name': 'admin' }
+            user = await Admin.findById(accountResult.user_id)
             user.account_type = 1
         } else if (accountResult.account_type == 2) {
             user = await SpecialistRepository.findById(accountResult.user_id)
             user.account_type = 2
         } else if (accountResult.account_type == 3) {
             user = await PatientRepository.findById(accountResult.user_id)
+            console.log(user, accountResult.user_id)
             user.account_type = 3
         }
     
         return res.json({
             "result": true,
-            "user": user
+            "user": await user
         })
     } catch (e) {
         return res.json({
--- controllers/ScheduleController.js
+++ controllers/ScheduleController.js
@@ -8,9 +8,13 @@ const getTodaySchedules = async (req, res) => {
     let day = ("0" + date_ob.getDate()).slice(-2);
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
     let year = date_ob.getFullYear();
+    var hours = date_ob.getHours();
+    var minutes = date_ob.getMinutes();
+    var timeToday = hours + ":" + minutes;
 
     const dateToday = year + "-" + month + "-" + day;
-    const schedules = await Schedule.showMany({date: dateToday, deleted_flag: false})
+    const scheds = await Schedule.showMany({deleted_flag: false})
+    const schedules = scheds.filter((sched) => sched.date >= dateToday && sched.time >= timeToday )
     return res.json({
         'result': true,
         'schedules': schedules
